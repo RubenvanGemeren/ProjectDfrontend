bb45837ad4a61a83022477f16bfa843d32e143f7
Scizivire <danilolima@live.nl>
2019-10-18 14:02:12 +0200

    omzetten naar javascript react + structuur project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css'
-import Header from './header'
-import Footer from './footer'
+// import Header from './header'
+import Router from './router'
 import {Grid, Row, Col} from 'react-bootstrap';
 import { BrowserRouter } from 'react-router-dom';
 var destination = document.querySelector('#root')
@@ -10,9 +9,10 @@ var destination = document.querySelector('#root')
 ReactDOM.render(
     <BrowserRouter>
         <div>
-            <Header />
+            {/* <Header /> */}
             <Router />
-            <Footer />
         </div>
     </BrowserRouter>
-)
+
+    , destination
+);
--- src/layout.js
+++ src/layout.js
@@ -1,54 +1,48 @@
-<!doctype html>
-<html lang="en">
+// <!doctype html>
+// <html lang="en">
 
-<head>
-  <title>Tafels</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+// <head>
+//   <title>Tafels</title>
+//   <!-- Required meta tags -->
+//   <meta charset="utf-8">
+//   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+//   <!-- Bootstrap CSS -->
+//   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
-  <style>
-    .jumbotron {
-      background-color: #e7d58d;
-      color: black;
 
-    }
+// </head>
 
-    /* Adds borders for tabs */
-    .tab-content {
-      border-left: 1px solid #ddd;
-      border-right: 1px solid #ddd;
-      border-bottom: 1px solid #ddd;
-    }
-    .nav-tabs {
-      margin-bottom: 0;
-    }
-    .TafelLayout {
-      padding-bottom: 65px;
-    }
-    .page-header{
-      background-color: #ceb967;
-    }
-    body{
-      background-color: #e7d58d;
-    }
-  </style>
-</head>
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import Table from './table';
+import './css/layout.css';
 
-<body>
-  <div class="container"> 
+class Layout extends Component{
+  constructor() {
+    super();
+
+
+  }
+
+  componentDidMount(){
+    window.scrollTo(0,0)
+  }
+
+  render() {
+    return(
+      <div class="container"> 
     <div class="page-header">
       <h1 class="text-center">Tafels</h1>
     </div>
 
     <div class="jumbotron">
       <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 TafelLayout">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 TafelLayout">          
           <h4>Tafel 1</h4>
+          <Link to="/tafel">
           <a href="#" class="btn btn-success btn-lg" role="button">Plaats bestelling &nbsp;<span class="glyphicon glyphicon-edit"></span></a>
+          </Link>
         </div>
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 TafelLayout">
           <h4>Tafel 2</h4>
@@ -80,23 +74,9 @@
         </div>
       </div>
     </div>
-
-
-
   </div>
+    )
+  }
+}
 
-
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-    crossorigin="anonymous"></script>
-</body>
-
-</html>
+export default Layout;
